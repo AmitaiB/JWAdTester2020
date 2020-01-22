003cffd6c334b04421356c9243a51c5f6938eea6
Amitai Blickstein <blickstein@gmail.com>
2020-01-22 16:49:13 -0500

    Pruned code files

--- JWAdTester2020/SwiftViewController.swift
+++ JWAdTester2020/SwiftViewController.swift
@@ -26,25 +26,3 @@ class SwiftViewController: UIViewController {
         }
     }
 }
-
-
-// MARK: - Helper method
-
-extension UIView {
-    /// Constrains the view to its superview, if it exists, using Autolayout.
-    /// - precondition: For player instances, JWP SDK 3.3.0 or higher.
-    @objc func constrainToSuperview() {
-        translatesAutoresizingMaskIntoConstraints = false
-        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[thisView]|",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["thisView": self])
-        
-        let verticalConstraints   = NSLayoutConstraint.constraints(withVisualFormat: "V:|[thisView]|",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["thisView": self])
-        
-        NSLayoutConstraint.activate(horizontalConstraints + verticalConstraints)
-    }
-}
---
+++ JWAdTester2020/UIView+Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Extensions.swift
+//  JWAdTester2020
+//
+//  Created by Amitai Blickstein on 1/22/20.
+//  Copyright © 2020 JWPlayer. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    /// Constrains the view to its superview, if it exists, using Autolayout.
+    /// - precondition: For player instances, JWP SDK 3.3.0 or higher.
+    @objc func constrainToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[thisView]|",
+                                                                   options: [],
+                                                                   metrics: nil,
+                                                                   views: ["thisView": self])
+        
+        let verticalConstraints   = NSLayoutConstraint.constraints(withVisualFormat: "V:|[thisView]|",
+                                                                   options: [],
+                                                                   metrics: nil,
+                                                                   views: ["thisView": self])
+        
+        NSLayoutConstraint.activate(horizontalConstraints + verticalConstraints)
+    }
+}
