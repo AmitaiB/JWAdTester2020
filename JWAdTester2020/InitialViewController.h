fd08891a5493e2341085b78ed2ce0f22b78ac45b
Amitai Blickstein <blickstein@gmail.com>
2020-01-22 16:27:38 -0500

    Initial Commit

---
+++ JWAdTester2020/InitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JWAdTester2020
+//
+//  Created by Max Mikheyenko on 1/7/15.
+//  Copyright (c) 2015 JWPlayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitialViewController : UIViewController
+
+@end
