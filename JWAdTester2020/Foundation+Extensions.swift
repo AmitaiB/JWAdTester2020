8c5b612cc98663fb68e6a139a38bca77f5d720bb
Amitai Blickstein <blickstein@gmail.com>
2020-01-22 19:31:18 -0500

    Adds Extensions and Validatable files
    sq

---
+++ JWAdTester2020/Foundation+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Foundation+Extensions.swift
+//  JWAdTester2020
+//
+//  Created by Amitai Blickstein on 1/22/20.
+//  Copyright © 2020 JWPlayer. All rights reserved.
+//
+
+import Foundation
